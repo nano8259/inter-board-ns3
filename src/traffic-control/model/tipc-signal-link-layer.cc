662eee1bee44b5d92bc718814a7aec6a822385b2
nano8259 <dashigl1999@gmail.com>
2022-06-28 15:50:13 +0800

    feat(TipcSignalLinkLayer): state machine

--- src/traffic-control/model/tipc-signal-link-layer.cc
+++ src/traffic-control/model/tipc-signal-link-layer.cc
@@ -24,13 +24,19 @@
 #include "ns3/packet.h"
 #include "ns3/socket.h"
 #include "ns3/queue-disc.h"
-#include <tuple>
+// #include <tuple>
+#include <sstream>
 
 namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE ("TipcSignalLinkLayer");
 
 NS_OBJECT_ENSURE_REGISTERED (TipcSignalLinkLayer);
+
+// const char* const
+// TipcSignalLinkLayer::TipcStateName[8] = { "LINK_ESTABLISHED", "LINK_ESTABLISHING", "LINK_RESET",
+//                                           "LINK_RESETTING", "LINK_PEER_RESET", "LINK_FAILINGOVER",
+//                                           "LINK_SYNCHING", "LAST_STATE" };
 
 TypeId
 TipcSignalLinkLayer::GetTypeId (void)
@@ -39,6 +45,10 @@ TipcSignalLinkLayer::GetTypeId (void)
     .SetParent<TrafficControlLayer> ()
     .SetGroupName ("Tipc")
     .AddConstructor<TipcSignalLinkLayer> ()
+    .AddTraceSource ("TipcState",
+                     "Trace TIPC state change of a TIPC signal link layer endpoint",
+                     MakeTraceSourceAccessor (&TipcSignalLinkLayer::m_stateTrace),
+                     "ns3::TipcSignalLinkLayer::TipcStatesTracedValueCallback")
   ;
   return tid;
 }
@@ -81,6 +91,176 @@ TipcSignalLinkLayer::NotifyNewAggregate ()
 {
   NS_LOG_FUNCTION (this);
   TrafficControlLayer::NotifyNewAggregate ();
+}
+
+uint32_t
+TipcSignalLinkLayer::LinkFsmEvent (int evt)
+{
+  NS_LOG_FUNCTION (this << evt);
+  uint32_t rc = 0;
+
+  switch (m_state)
+    {
+      case LINK_RESETTING:
+        switch (evt)
+          {
+            case LINK_PEER_RESET_EVT:
+              m_state = LINK_PEER_RESET;
+              break;
+            case LINK_RESET_EVT:
+              m_state = LINK_RESET;
+              break;
+            case LINK_FAILURE_EVT:
+            case LINK_FAILOVER_BEGIN_EVT:
+            case LINK_ESTABLISH_EVT:
+            case LINK_FAILOVER_END_EVT:
+            case LINK_SYNCH_BEGIN_EVT:
+            case LINK_SYNCH_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      case LINK_RESET:
+        switch (evt)
+          {
+            case LINK_PEER_RESET_EVT:
+              m_state = LINK_ESTABLISHING;
+              break;
+            case LINK_FAILOVER_BEGIN_EVT:
+              m_state = LINK_FAILINGOVER;
+              break;
+            case LINK_FAILURE_EVT:
+            case LINK_RESET_EVT:
+            case LINK_ESTABLISH_EVT:
+            case LINK_FAILOVER_END_EVT:
+              break;
+            case LINK_SYNCH_BEGIN_EVT:
+            case LINK_SYNCH_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      case LINK_PEER_RESET:
+        switch (evt)
+          {
+            case LINK_RESET_EVT:
+              m_state = LINK_ESTABLISHING;
+              break;
+            case LINK_PEER_RESET_EVT:
+            case LINK_ESTABLISH_EVT:
+            case LINK_FAILURE_EVT:
+              break;
+            case LINK_SYNCH_BEGIN_EVT:
+            case LINK_SYNCH_END_EVT:
+            case LINK_FAILOVER_BEGIN_EVT:
+            case LINK_FAILOVER_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      case LINK_FAILINGOVER:
+        switch (evt)
+          {
+            case LINK_FAILOVER_END_EVT:
+              m_state = LINK_RESET;
+              break;
+            case LINK_PEER_RESET_EVT:
+            case LINK_RESET_EVT:
+            case LINK_ESTABLISH_EVT:
+            case LINK_FAILURE_EVT:
+              break;
+            case LINK_FAILOVER_BEGIN_EVT:
+            case LINK_SYNCH_BEGIN_EVT:
+            case LINK_SYNCH_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      case LINK_ESTABLISHING:
+        switch (evt)
+          {
+            case LINK_ESTABLISH_EVT:
+              m_state = LINK_ESTABLISHED;
+              break;
+            case LINK_FAILOVER_BEGIN_EVT:
+              m_state = LINK_FAILINGOVER;
+              break;
+            case LINK_RESET_EVT:
+              m_state = LINK_RESET;
+              break;
+            case LINK_FAILURE_EVT:
+            case LINK_PEER_RESET_EVT:
+            case LINK_SYNCH_BEGIN_EVT:
+            case LINK_FAILOVER_END_EVT:
+              break;
+            case LINK_SYNCH_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      case LINK_ESTABLISHED:
+        switch (evt)
+          {
+            case LINK_PEER_RESET_EVT:
+              m_state = LINK_PEER_RESET;
+              rc |= TIPC_LINK_DOWN_EVT;
+              break;
+            case LINK_FAILURE_EVT:
+              m_state = LINK_RESETTING;
+              rc |= TIPC_LINK_DOWN_EVT;
+              break;
+            case LINK_RESET_EVT:
+              m_state = LINK_RESET;
+              break;
+            case LINK_ESTABLISH_EVT:
+            case LINK_SYNCH_END_EVT:
+              break;
+            case LINK_SYNCH_BEGIN_EVT:
+              m_state = LINK_SYNCHING;
+              break;
+            case LINK_FAILOVER_BEGIN_EVT:
+            case LINK_FAILOVER_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      case LINK_SYNCHING:
+        switch (evt)
+          {
+            case LINK_PEER_RESET_EVT:
+              m_state = LINK_PEER_RESET;
+              rc |= TIPC_LINK_DOWN_EVT;
+              break;
+            case LINK_FAILURE_EVT:
+              m_state = LINK_RESETTING;
+              rc |= TIPC_LINK_DOWN_EVT;
+              break;
+            case LINK_RESET_EVT:
+              m_state = LINK_RESET;
+              break;
+            case LINK_ESTABLISH_EVT:
+            case LINK_SYNCH_BEGIN_EVT:
+              break;
+            case LINK_SYNCH_END_EVT:
+              m_state = LINK_ESTABLISHED;
+              break;
+            case LINK_FAILOVER_BEGIN_EVT:
+            case LINK_FAILOVER_END_EVT:
+            default:
+              goto illegal_evt;
+          }
+        break;
+      default:
+        std::stringstream errSs;
+        errSs << "Unknown FSM state " << m_state << "in " << this << std::endl;
+        NS_FATAL_ERROR (errSs.str ());
+    }
+  return rc;
+illegal_evt:
+  std::stringstream errSs;
+  errSs << "Illegal FSM event" << evt << "in state" << m_state << "on link " << this << std::endl;
+  NS_FATAL_ERROR (errSs.str ());
+  return rc;
 }
 
 } // namespace ns3
